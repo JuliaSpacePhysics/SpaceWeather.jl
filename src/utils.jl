245189ee01dae8371184ca1ad3817fd94d3c8c87
Beforerr <zzj956959688@gmail.com>
2026-01-07 13:32:35 -0800

    refactor: Clean up utility functions

--- src/utils.jl
+++ src/utils.jl
@@ -12,7 +12,7 @@ function datadir()
         DATA_DIR[] = joinpath(first(DEPOT_PATH), "spaceweather")
     end
     mkpath(DATA_DIR[])
-    DATA_DIR[]
+    return DATA_DIR[]
 end
 
 """
@@ -20,7 +20,7 @@ end
 
 Download a file from `url` to `dest` if it doesn't exist or is older than `min_age`.
 """
-function download_file(url, dest; update=false, min_age=Hour(3))
+function download_file(url, dest; update = false, min_age = Hour(3))
     needs_download = !isfile(dest)
     if !needs_download && update
         file_age = now() - unix2datetime(mtime(dest))
@@ -30,20 +30,5 @@ function download_file(url, dest; update=false, min_age=Hour(3))
         mkpath(dirname(dest))
         Downloads.download(url, dest)
     end
-    dest
+    return dest
 end
-
-"""
-    parse_datetime(year, month, day, hour=0, minute=0)
-
-Create a DateTime from components.
-"""
-parse_datetime(year, month, day, hour=0, minute=0) =
-    DateTime(year, month, day, hour, minute)
-
-"""
-    parse_date_doy(year, doy, hour=0)
-
-Create a DateTime from year, day-of-year, and hour.
-"""
-parse_date_doy(year, doy, hour=0) = DateTime(year) + Day(doy - 1) + Hour(hour)
