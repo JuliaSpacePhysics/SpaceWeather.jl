ca13d4771a9c96b847d7c16f0611844653027371
Beforerr <zzj956959688@gmail.com>
2026-01-07 15:15:01 -0800

    feat: Add GOES-R XRS data source with X-ray flux accessors

--- src/SpaceWeather.jl
+++ src/SpaceWeather.jl
@@ -5,17 +5,21 @@ using Downloads
 using DelimitedFiles, CSV
 using DataFrames
 using AxisKeys
+using NCDatasets
 
 # Core utilities
 include("utils.jl")
 
 # Data sources
 include("celestrak.jl")
+include("goesr.jl")
 include("accessors.jl")
 
 export celestrak
+export goesr_xrs
 
 # High-level accessors
 export Kp, Ap
+export xrsa, xrsb
 
 end
--- src/accessors.jl
+++ src/accessors.jl
@@ -17,3 +17,21 @@ Get Ap index data from `source`.
 function Ap(args...; source = :celestrak, kwargs...)
     return source == :celestrak && celestrak(args...; kwargs...).Ap
 end
+
+"""
+    xrsa(id, args...; kwargs...)
+
+Get GOES-R XRS-A (0.05-0.4 nm) X-ray flux data.
+"""
+function xrsa(id, args...; kwargs...)
+    return goesr_xrs(id, args...; kwargs...).xrsa_flux
+end
+
+"""
+    xrsb(id, args...; kwargs...)
+
+Get GOES-R XRS-B (0.1-0.8 nm) X-ray flux data.
+"""
+function xrsb(satellite, args...; kwargs...)
+    return goesr_xrs(satellite, args...; kwargs...).xrsb_flux
+end
